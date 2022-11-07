3d0ba30c500feb79e2bb041676e98c82ec08e7bf
kent-hub <ke.tanabe@gmail.com>
2022-11-07 13:32:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Example code with Wechat Work API
+This is an example python code to send a message to a 'Wechat Work' (企业微信).
+
+## Installation or Getting Started
+```
+pip install -r requirements.txt
+python main.py
+```
+
+## Reference
++ [企业微信 开发者中心](https://developer.work.weixin.qq.com/) - [发送应用消息](https://developer.work.weixin.qq.com/document/path/90236)
---
+++ main.py
@@ -0,0 +1,48 @@
+"""Example code with Wechat Work API
+This is an example python code to send a message to a 'Wechat Work' (企业微信).
+"""
+
+import os
+import sys
+import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def GetAccessToken():
+    """
+    Connect to the We Chat Work API and get an access token.
+    """
+    CORP_ID = os.environ['CORP_ID']
+    CORP_SECRET = os.environ['CORP_SECRET']
+
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={CORP_ID}&corpsecret={CORP_SECRET}'
+    res = requests.post(url)
+    res_json = json.loads(res.text)
+    token = res_json.get('access_token')
+
+    return token
+
+token = GetAccessToken()
+if token == None:
+    print('ERROR! Unable to get token, please check your corp ID or corp secret.')
+    sys.exit()
+
+agentid, touser, msgtype = '999999', 'test_user', 'text'
+json_data = {
+    "agentid": agentid,
+    "touser": touser,
+    "msgtype": msgtype,
+    "text" : {
+       "content" : "Test!\n<a href=\"http://example.com\">example.com</a>"
+   },
+}
+
+url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={token}'
+res = requests.post(url, data = json.dumps(json_data))
+if res.status_code != 200:
+    print(f'ERROR! Status code: {res.status_code}')
+    sys.exit()
+
+print(res.text)
